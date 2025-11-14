e04576e28ff84b88203b03c8a6ad6dce78cdb705
pihusanghai12-design <pihusanghai12@gmail.com>
2025-11-14 17:14:38 +0545

    simple calculator

---
+++ Simple calculator.js
@@ -0,0 +1,16 @@
+var number1= 98;
+var number2= 54;
+var add=number1+number2;
+console.log(add);
+var num1=67;
+var num2=45;
+var subtraction=num1-num2;
+console.log(subtraction);
+var no1=56;
+var no2=4;
+var multiply=no1*no2;
+console.log(multiply);
+var a=10;
+var b=2;
+var divide=a/b
+console.log(divide);
