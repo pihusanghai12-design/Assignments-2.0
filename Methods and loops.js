3ded19b6400364956f8ce3c64a85660c0404d339
pihusanghai12-design <pihusanghai12@gmail.com>
2025-11-15 20:17:09 +0545

    Methods and Loop

---
+++ Methods and loops.js
@@ -0,0 +1,7 @@
+var number ="3,9,7,5";
+for(i=1; i<4; i++) {
+    number+= 1+"<br>"
+}
+console.log("The random value is"+Math.floor(Math.random()*100));
+var Date= new Date();
+console.log("Today's Date is"+date.getDate());
