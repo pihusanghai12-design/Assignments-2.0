8b7997fed90616302c25f22fe2d16bfe55becf70
pihusanghai12-design <pihusanghai12@gmail.com>
2025-12-19 16:39:57 +0545

    HTML Dom assignment

---
+++ HTML Dom.js
@@ -0,0 +1,6 @@
+var fruits=["apple","Mango","Banana"]
+console.log(fruits.pop());
+console.log(fruits);
+var stationery = ["pen","Pencil","eraser","Sharpener","ruler"];
+console.log(stationery.join("*"));
+console.log(stationery);
